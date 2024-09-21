f1555ef352ce8bb9d78e4d5e441a2684c600024c
Manda Putra <mandaputra@Mandas-MacBook-Pro.local>
2024-09-22 05:09:06 +0700

    feat: rapi rapi logo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <main class="w-svw px-4 flex flex-col justify-center font-serif">
-    <header class="my-4 w-full text-center">
+    <header
+      class="my-8 w-full text-center flex flex-col justify-center items-center"
+    >
+      <img src="/circle.svg" height="50" width="50" alt="" />
       <h1 class="text-3xl font-bold">MomDad</h1>
       <p>A Studio</p>
     </header>
-    <section id="hero" class="mb-4 flex flex-col justify-center items-center">
+    <section id="hero" class="mb-8 flex flex-col justify-center items-center">
       <img
         class="max-h-[900px]"
         src="/pexels-bruna-fossile-183845208-28462495.jpg"
