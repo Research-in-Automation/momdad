c71ebb6f89a4cd5cf9d4d211bbacdce7cc7e216e
Manda Putra <mandaputra@Mandas-MacBook-Pro.local>
2024-09-22 05:05:04 +0700

    feat: add contact us

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
-  <main class="w-svw px-4 flex flex-col justify-center">
+  <main class="w-svw px-4 flex flex-col justify-center font-serif">
     <header class="my-4 w-full text-center">
       <h1 class="text-3xl font-bold">MomDad</h1>
       <p>A Studio</p>
     </header>
-    <section
-      id="hero"
-      class="mb-4 flex flex-col justify-center items-center max-w-600px"
-    >
+    <section id="hero" class="mb-4 flex flex-col justify-center items-center">
       <img
-        src="../public/pexels-bruna-fossile-183845208-28462495.jpg"
+        class="max-h-[900px]"
+        src="/pexels-bruna-fossile-183845208-28462495.jpg"
         alt="pexels bruna fossile"
       />
     </section>
     <footer class="text-center">
       <p class="font-bold">Contact Us</p>
-      <ul>
+      <ul class="text-gray-500">
         <li>+6285 9459 80820</li>
         <li>@momdad</li>
         <li>Momdad Studio</li>
