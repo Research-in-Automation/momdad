17723b497103f9c4b118d7c9550859b8ce13f35b
Manda Putra <mandaputra@Mandas-MacBook-Pro.local>
2024-09-22 04:54:59 +0700

    feat: studio momdad

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="w-svw px-4 flex flex-col justify-center">
+    <header class="my-4 w-full text-center">
+      <h1 class="text-3xl font-bold">MomDad</h1>
+      <p>A Studio</p>
+    </header>
+    <section
+      id="hero"
+      class="mb-4 flex flex-col justify-center items-center max-w-600px"
+    >
+      <img
+        src="../public/pexels-bruna-fossile-183845208-28462495.jpg"
+        alt="pexels bruna fossile"
+      />
+    </section>
+    <footer class="text-center">
+      <p class="font-bold">Contact Us</p>
+      <ul>
+        <li>+6285 9459 80820</li>
+        <li>@momdad</li>
+        <li>Momdad Studio</li>
+      </ul>
+    </footer>
+  </main>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
