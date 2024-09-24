75d5d5c0de1aa40f84cc17e5b87d9f6c44b09102
mandaputtra <mandaputra8@gmail.com>
2024-09-24 22:20:50 +0700

    feat: add logos

--- src/App.vue
+++ src/App.vue
@@ -3,42 +3,41 @@
     <header
       class="my-8 w-full text-center flex flex-col justify-center items-center"
     >
-      <img src="/circle.svg" height="50" width="50" alt="" />
-      <h1 class="text-3xl font-bold">MomDad</h1>
-      <p>A Studio</p>
+      <img src="/LOGO2.webp" width="300" alt="" />
     </header>
     <section id="hero" class="mb-8 flex flex-col justify-center items-center">
-      <img
-        class="max-h-[900px]"
-        src="/pexels-bruna-fossile-183845208-28462495.jpg"
-        alt="pexels bruna fossile"
-      />
+      <section class="relative">
+        <img
+          class="absolute top-2/4 md:right-[17%]"
+          style="transform: translate(0, -50%)"
+          src="../public/LOGO_WHITE.webp"
+          width="400"
+          alt="logo minimal"
+        />
+        <img
+          class="max-h-[900px]"
+          src="../public/pexels-romakaiuk-11876440.jpg"
+          alt="pexels bruna fossile"
+        />
+      </section>
     </section>
     <footer class="text-center">
       <p class="font-bold mb-4">Contact Us</p>
       <ul class="text-gray-500 flex flex-col mb-4">
         <li>
-          <a href="tel:+6285945980820"> +6285 9459 80820 </a>
+          <a href="tel:+6281325460020">+62 8132 5460 020</a>
         </li>
         <li>
-          <a rel="stylesheet" href="mailto:talk@momdad.id"> talk@momdad.id </a>
+          <a rel="stylesheet" href="mailto:studio@momdad.id"
+            >studio@momdad.id</a
+          >
         </li>
       </ul>
       <ul
         class="text-gray-500 flex w-full items-center justify-center gap-2 mb-4"
       >
         <li>
-          <a href="https://facebook.com">
-            <img
-              width="15"
-              height="15"
-              src="/facebook-2-logo-svgrepo-com.svg"
-              alt=""
-            />
-          </a>
-        </li>
-        <li>
-          <a href="https://instagram.com">
+          <a href="https://instagram.com/momdad___________">
             <img
               width="15"
               height="15"
